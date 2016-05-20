a5191a65faede2029a4be3d9efb4913af7ffbcb8
David Mulhern <david@mulhern.me>
2016-05-20 00:32:30 -0700

    Adjusting the App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, Directive, ElementRef, Renderer} from '@angular/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {Http} from '@angular/http';
 
-import {DougComponent} from '../sprites/puppets/doug/doug.component';
+import {DougComponent} from '../sprites/doug/doug.component';
 
 /////////////////////////
 // ** Example Directive
@@ -24,17 +24,29 @@ export class XLarge {
 // ** Example Components
 @Component({
   selector: 'home',
+  directives: [
+    DougComponent
+  ],
   template: `
-    <div>This is the "Home" page</div>
-  `
+    <div class="water"><doug-sprite></doug-sprite></div>
+  `,
+  styles: [`
+   .water{padding:80px;}
+  `],
 })
 export class Home { }
 
 @Component({
   selector: 'about',
+  directives: [
+  ],
   template: `
-  <div>This is the "About" page</div>
-  `
+  <div class="stars"><a href="https://github.com/sonipa/doug">doug github</a></div>
+  `,
+  styles: [`
+   .stars{background:#999;color:#fff;padding:100px;}
+   a {color:#fff;} a:hover{color:yellow;}
+  `],
 })
 export class About { }
 
@@ -43,51 +55,30 @@ export class About { }
 @Component({
   selector: 'app', // <app></app>
   directives: [
-    ...ROUTER_DIRECTIVES,
-    XLarge,
-    DougComponent
+    ...ROUTER_DIRECTIVES
   ],
   styles: [`
-    * { padding:0; margin:0; }
-    #universal { text-align:center; font-weight:bold; padding:15px 0; }
-    nav { background:#158126; min-height:40px; border-bottom:5px #046923 solid; }
-    nav a { font-weight:bold; text-decoration:none; color:#fff; padding:20px; display:inline-block; }
-    nav a:hover { background:#00AF36; }
-    .hero-universal { min-height:500px; display:block; padding:20px; background: url('/src/logo.png') no-repeat center center; }
-    .inner-hero { background: rgba(255, 255, 255, 0.75); border:5px #ccc solid; padding:25px; }
-    .router-link-active { background-color: #00AF36; }
-    blockquote { border-left:5px #158126 solid; background:#fff; padding:20px 20px 20px 40px; }
-    blockquote::before { left: 1em; }
-    main { padding:20px 0; }
-    pre { font-size:12px; }
+   pre {
+   border-top:#ccc 6px solid;
+   background:#f2f2f2;
+   }
   `],
   template: `
-  <h3 id="universal">Angular2 Universal</h3>
-  <nav>
-    <a [routerLink]=" ['./Home'] ">Home</a>
-    <a [routerLink]=" ['./About'] ">About</a>
-  </nav>
-  <doug-sprite></doug-sprite>
-  <div class="hero-universal">
-    <div class="inner-hero">
-      <div>
-        <span x-large>Universal JavaScript {{ title }}!</span>
-      </div>
-
-      Two-way binding: <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
-      <br><br>
-
-      <strong>Async data call return value:</strong>
-      <pre>{{ data | json }}</pre>
-
-      <strong>Router-outlet:</strong>
-      <main>
-        <router-outlet></router-outlet>
-      </main>
-
-      <blockquote>{{ server }}</blockquote>
-    </div>
-  </div>
+  <main>
+    <router-outlet></router-outlet>
+  </main>
+  
+    
+  
+<pre>
+async data:
+{{ data | json }}
+about controller: <a [routerLink]=" ['./About'] ">About</a>
+home link: <a [routerLink]=" ['./Home'] ">Home</a>
+title: {{ title }}
+binding: two way binding: <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
+server message: {{ server }}
+</pre>
   `
 })
 @RouteConfig([
---
+++ src/sprites/doug/doug.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'doug-sprite',
+  template: 'doug'
+})
+export class DougComponent { 
+	
+}
